f6778deeb97b26fef16c2cb707e0cc522fab01a2
jwpark97114 <jwpark97114@gmail.com>
2020-06-21 03:09:48 -0700

    intent("name")으로 글자 받아서 띄우기

--- app/src/main/java/kr/co/namu/intentpractice_20200621/ThirdActivity.kt
+++ app/src/main/java/kr/co/namu/intentpractice_20200621/ThirdActivity.kt
@@ -2,10 +2,16 @@ package kr.co.namu.intentpractice_20200621
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+//        들어올 때 message란 이름의 string 이 있는지 확인 => 결과를 변수에 저장
+        val receivedMessage = intent.getStringExtra("message")
+
+        messageTxt.text = receivedMessage
     }
 }
