6b8fd9cf046fde3a01fe528bacef2941f2880a3f
jwpark97114 <jwpark97114@gmail.com>
2020-06-21 02:04:57 -0700

    다른 화면으로 이동 처리 - Intent 메모 참고

--- app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
+++ app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
@@ -1,11 +1,20 @@
 package kr.co.namu.intentpractice_20200621
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstActivityBtn.setOnClickListener {
+//            Intent 객체를 생성 (비행기 티켓 발권과 비슷)
+            val myIntent = Intent(this, FirstActivity::class.java)
+//            실제로 인텐트 사용(비행기 탑승 or 출발)
+            startActivity(myIntent)
+        }
     }
 }
